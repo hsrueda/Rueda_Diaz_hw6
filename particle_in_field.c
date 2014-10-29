641f840b99fc92bb20ecbcdbd80dcc80c661947f
Doctus <doctus5@gmail.com>
2014-10-29 15:23:17 -0500

    1.0, definición de variables

---
+++ particle_in_field.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(int argc, char **argv){
+
+  float Bo=3*(10**(-15));
+  float Rt=6378.1;
+  float q=1.6*(10**-19);
+  float m=1.67*(10**-27);
+  float c=299792458.0;
+
+
+}
+
