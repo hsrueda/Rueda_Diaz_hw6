6ff92fca3e45d96ed8a1982d02810ef58cc2e62c
Doctus <doctus5@gmail.com>
2014-10-30 15:59:20 -0500

    1.2, Definicion de Métodos

--- particle_in_field.c
+++ particle_in_field.c
@@ -3,15 +3,51 @@
 #include <string.h>
 #include <math.h>
 
-int main(int argc, char **argv){
+#define Rt 6378137
+#define m 1.67*(10**-27)
+#define c 299792458
+#define c2 8.9875518*(10**16)
+#define q 1.6*(10**(-19))
+#define Bo 3*(10**(-5))
+#define PI 3.14159265
 
-  float Bo=3*(10**(-15));
-  float Rt=6378.1;
-  float q=1.6*(10**-19);
-  float m=1.67*(10**-27);
-  float c=299792458.0;
+
+int main(int argc, char **argv){
+//Definicion de variables
+  double Ec0=atof(argv[1]);
+  double alpha=atof(argv[2]);
+  double Ecj=Ec0*e*1000000;
+  double gamma=1+Ecj/(m*c2);
+  double vi=c*sqrt(1-(1/pow(gamma,2)));
+  double t0=0.0;
+  double tf=100.0;
+  double npuntos=(tf-t0)/(2*PI*gamma*m/(e*Bo));
+
+}
+
+double funcion1(double x, double y, double z, double x_prima, double y_prima, double z_prima, double gm){
+
+  double r=pow(x,2)+pow(y,2)+pow(z,2);
+  double func1=e*(-Bo*(pow(Rt,3)))*(z_prima*(3*y*z)-x_prima*(2*z*(z-x)*(x-y)*y))/(m*gama*pow(r,5.0/2.0));
+  return func1; 
+
+}
+
+double funcion2(double x, double y, double z, double x_prima, double y_prima, double z_prima, double gm){
+
+  double r=pow(x,2)+pow(y,2)+pow(z,2);
+  double func2=e*(-Bo*(pow(Rt,3)))*(z_prima*(3*x*z)-x_prima*(2*z*(z-x)*(x-y)*y))/(m*gama*pow(r,5.0/2.0));
+  return func2;
+}
+
+double funcion3(double x, double y, double z, double x_prima, double y_prima, double z_prima, double gm){
+
+  double r=pow(x,2)+pow(y,2)+pow(z,2);
+  double func3=e*(-Bo*(pow(Rt,3)))*(z_prima*(3*x*z)-x_prima*(2*z*(z-x)*(x-y)*y))/(m*gama*pow(r,5.0/2.0));
+  return func3;
+}
 
 "sebastianrosales"
 
-}
+
 
